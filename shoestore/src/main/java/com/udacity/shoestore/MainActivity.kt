bd5ac9a3d868521c7adf45e11d81b978160abd51
Ayşe Adanç <adancaysee@gmail.com>
2022-12-24 21:56:28 +0100

    Initial commit

---
+++ shoestore/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.udacity.shoestore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.udacity.shoestore.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        NavigationUI.setupActionBarWithNavController(this, getNavController(), null)
+    }
+
+    private fun getNavController(): NavController {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        return navHostFragment.navController
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return getNavController().navigateUp()
+    }
+}
