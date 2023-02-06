14801f33cff3919566b43aabd202534ece28db76
Ayşe Adanç <adancaysee@gmail.com>
2023-02-06 16:32:55 +0100

    retrofit api refactoring

--- asteroidradar/src/main/java/com/udacity/asteroidradar/network/NEoWsApi.kt
+++ asteroidradar/src/main/java/com/udacity/asteroidradar/network/NEoWsApi.kt
@@ -2,7 +2,9 @@ package com.udacity.asteroidradar.network
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.udacity.asteroidradar.BuildConfig
 import com.udacity.asteroidradar.util.Constants.BASE_URL
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
@@ -14,13 +16,26 @@ private val moshi = Moshi.Builder()
 
 private val retrofitWithScalar: Retrofit = Retrofit.Builder()
     .baseUrl(BASE_URL)
+    .client(getRetrofitClient())
     .addConverterFactory(ScalarsConverterFactory.create())
     .build()
 
 private val retrofitWithMoshi: Retrofit = Retrofit.Builder()
     .baseUrl(BASE_URL)
+    .client(getRetrofitClient())
     .addConverterFactory(MoshiConverterFactory.create(moshi))
     .build()
+
+private fun getRetrofitClient() =  OkHttpClient.Builder()
+    .addInterceptor { chain ->
+        val url = chain
+            .request()
+            .url()
+            .newBuilder()
+            .addQueryParameter("api_key", BuildConfig.NASA_API_KEY)
+            .build()
+        chain.proceed(chain.request().newBuilder().url(url).build())
+    }.build()
 
 object NEoWsApi {
     val remoteAsteroidDataSource: RemoteAsteroidDataSource by lazy {
--- asteroidradar/src/main/java/com/udacity/asteroidradar/network/RemoteDataSources.kt
+++ asteroidradar/src/main/java/com/udacity/asteroidradar/network/RemoteDataSources.kt
@@ -1,13 +1,11 @@
 package com.udacity.asteroidradar.network
 
-import com.udacity.asteroidradar.BuildConfig
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface RemoteAsteroidDataSource {
     @GET("neo/rest/v1/feed")
     suspend fun getAsteroids(
-        @Query("api_key") apiKey: String = BuildConfig.NASA_API_KEY,
         @Query("start_date") startDate: String?,
         @Query("end_date") endDate: String?,
     ): String
@@ -15,5 +13,5 @@ interface RemoteAsteroidDataSource {
 
 interface RemotePictureOfDayDataSource {
     @GET("planetary/apod")
-    suspend fun getPictureOfDay(@Query("api_key") apiKey: String = BuildConfig.NASA_API_KEY): NetworkPictureOfDay
+    suspend fun getPictureOfDay(): NetworkPictureOfDay
 }
