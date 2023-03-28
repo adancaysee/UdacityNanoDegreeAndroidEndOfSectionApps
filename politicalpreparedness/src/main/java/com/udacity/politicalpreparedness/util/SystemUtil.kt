f9b3f6431569131cdd3ed451e5ad5d5bdd17a4bd
Ayşe Adanç <adancaysee@gmail.com>
2023-03-28 11:11:13 +0200

    internet permission check

--- politicalpreparedness/src/main/java/com/udacity/politicalpreparedness/util/SystemUtil.kt
+++ politicalpreparedness/src/main/java/com/udacity/politicalpreparedness/util/SystemUtil.kt
@@ -1,12 +1,31 @@
 package com.udacity.politicalpreparedness.util
 
-import android.app.Application
 import android.content.Context
-import android.content.SharedPreferences
 import android.content.pm.PackageManager
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import androidx.core.content.ContextCompat
 
-fun Context.hasPermission(permission: String) =
-    ContextCompat.checkSelfPermission(
+fun Context.hasPermission(permission: String): Boolean {
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) return true
+    return ContextCompat.checkSelfPermission(
         this, permission
     ) == PackageManager.PERMISSION_GRANTED
+}
+
+fun Context.isInternetAvailable(): Boolean {
+    val manager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+        manager.getNetworkCapabilities(manager.activeNetwork)?.let {
+            it.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+                    it.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
+                    it.hasTransport(NetworkCapabilities.TRANSPORT_BLUETOOTH) ||
+                    it.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) ||
+                    it.hasTransport(NetworkCapabilities.TRANSPORT_VPN)
+        } ?: false
+    else
+        @Suppress("DEPRECATION")
+        manager.activeNetworkInfo?.isConnectedOrConnecting == true
+}
+
