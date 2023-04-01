63be37bd4b9b177d38b8feac7e70d3bcae70d82b
Ayşe Adanç <adancaysee@gmail.com>
2023-04-01 13:49:57 +0200

    removed some modules

--- project4/src/main/java/com/udacity/project4/util/map/GeofenceHelper.kt
+++
@@ -1,5 +0,0 @@
-package com.udacity.project4.util.map
-
-object GeofenceHelper {
-    const val GEOFENCE_RADIUS = 200.0
-}
--- shoestore/src/main/java/com/udacity/shoestore/model/Shoe.kt
+++
@@ -1,9 +0,0 @@
-package com.udacity.shoestore.model
-
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-
-@Parcelize
-data class Shoe(var name: String, var size: Int, var company: String, var description: String,
-                val images: List<Int> = mutableListOf()) : Parcelable
